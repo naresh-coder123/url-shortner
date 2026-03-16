85daf7168b4a6224c64adeb36a6941d24b5d1ae3
naresh-coder123 <nc9676@gmail.com>
2026-03-16 19:08:14 +0530

    Remove server proxy settings from vite.config.js
    Removed server proxy configuration from Vite.

--- frontend/url-shortner-service/client/vite.config.js
+++ frontend/url-shortner-service/client/vite.config.js
@@ -5,14 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000', 
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+ 
   plugins: [react(), tailwindcss()],
 });
